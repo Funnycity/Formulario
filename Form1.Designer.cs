0c1d601b3b6b96d5205adc3ff203e1e445f56e0a
Funnycity <raboymota@gmail.com>
2022-05-18 09:38:11 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNom1 = new System.Windows.Forms.TextBox();
+            this.txtNom2 = new System.Windows.Forms.TextBox();
+            this.txtApe1 = new System.Windows.Forms.TextBox();
+            this.txtApe2 = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtCalle = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtInter = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cedula";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primer Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Segundo Nombre";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Primer Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Segundo Apellido";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Fecha de Nacimiento";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(246, 360);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Direccion";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(87, 398);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Calle";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(78, 436);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 12);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Numero";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(54, 471);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 12);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Interseccion";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(70, 275);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 12);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Telefono";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(85, 316);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 12);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Email";
+            // 
+            // txtNom1
+            // 
+            this.txtNom1.Location = new System.Drawing.Point(140, 86);
+            this.txtNom1.Name = "txtNom1";
+            this.txtNom1.Size = new System.Drawing.Size(270, 20);
+            this.txtNom1.TabIndex = 13;
+            // 
+            // txtNom2
+            // 
+            this.txtNom2.Location = new System.Drawing.Point(140, 122);
+            this.txtNom2.Name = "txtNom2";
+            this.txtNom2.Size = new System.Drawing.Size(270, 20);
+            this.txtNom2.TabIndex = 14;
+            // 
+            // txtApe1
+            // 
+            this.txtApe1.Location = new System.Drawing.Point(140, 159);
+            this.txtApe1.Name = "txtApe1";
+            this.txtApe1.Size = new System.Drawing.Size(270, 20);
+            this.txtApe1.TabIndex = 15;
+            // 
+            // txtApe2
+            // 
+            this.txtApe2.Location = new System.Drawing.Point(140, 190);
+            this.txtApe2.Name = "txtApe2";
+            this.txtApe2.Size = new System.Drawing.Size(270, 20);
+            this.txtApe2.TabIndex = 16;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(140, 231);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(270, 20);
+            this.txtFecha.TabIndex = 17;
+            // 
+            // txtCalle
+            // 
+            this.txtCalle.Location = new System.Drawing.Point(140, 390);
+            this.txtCalle.Name = "txtCalle";
+            this.txtCalle.Size = new System.Drawing.Size(270, 20);
+            this.txtCalle.TabIndex = 19;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(140, 428);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(270, 20);
+            this.txtNum.TabIndex = 20;
+            // 
+            // txtInter
+            // 
+            this.txtInter.Location = new System.Drawing.Point(140, 463);
+            this.txtInter.Name = "txtInter";
+            this.txtInter.Size = new System.Drawing.Size(270, 20);
+            this.txtInter.TabIndex = 21;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(140, 267);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(270, 20);
+            this.txtTel.TabIndex = 22;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(140, 308);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(270, 20);
+            this.txtEmail.TabIndex = 23;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(89, 521);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(144, 23);
+            this.btnAceptar.TabIndex = 24;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(308, 521);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(144, 23);
+            this.btnCancelar.TabIndex = 25;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(140, 57);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(270, 20);
+            this.txtCedula.TabIndex = 26;
+            this.txtCedula.TextChanged += new System.EventHandler(this.txtCedula_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(544, 556);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtInter);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtCalle);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtApe2);
+            this.Controls.Add(this.txtApe1);
+            this.Controls.Add(this.txtNom2);
+            this.Controls.Add(this.txtNom1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtNom1;
+        private System.Windows.Forms.TextBox txtNom2;
+        private System.Windows.Forms.TextBox txtApe1;
+        private System.Windows.Forms.TextBox txtApe2;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtCalle;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtInter;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtCedula;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCedula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtApe1.Text = "";
+            txtApe2.Text = "";
+            txtCalle.Text = "";
+            txtCedula.Text = "";
+            txtEmail.Text = "";
+            txtFecha.Text = "";
+            txtInter.Text = "";
+            txtNom1.Text = "";
+            txtNom2.Text = "";
+            txtNum.Text = "";
+            txtTel.Text = "";
+
+
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            int cedula = Convert.ToInt32(txtCedula.Text);
+            String nom1 = txtNom1.Text;
+            String nom2 = txtNom2.Text;
+            String ape1 = txtApe1.Text;
+            String ape2 = txtApe2.Text;
+            String fecha = txtFecha.Text;
+            String calle = txtCalle.Text;
+            int num = Convert.ToInt32(txtNum.Text);
+            String inter = txtInter.Text;
+            int tel = Convert.ToInt32(txtTel.Text);
+            String email = txtEmail.Text;
+
+            if (txtCedula.Text == "" || txtApe1.Text == "" || txtCalle.Text == "" || txtEmail.Text == "" || txtFecha.Text == "" || txtNom1.Text == "" ||
+                txtNum.Text == "" || txtEmail.Text == "" || txtInter.Text == "")
+            {
+                MessageBox.Show("Uno de los campos está vació");
+            }
+            else
+            {
+
+                MessageBox.Show("Tu cedula es: " + cedula + "\nTu primer nombre es: " + nom1 + "\nTu segundo nombre es: " + nom2 +
+                    "\nTu primer apellido es: " + ape1 + "\nTu segundo apellido es: " + ape2 + "\nTu fecha de nacimiento es: " + fecha + "\nTu email es: " + email +
+                    "\nLa direccion de tu casa es: " + "        \nCalle: " + calle + "      \nNúmero: " + num + "       \nIntersección: " + inter);
+
+            }
+
+
+
+        }
+    }
+}
